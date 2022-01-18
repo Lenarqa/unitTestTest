eadacbbaa4ef60a79c1c54e509eb3e401dcaaa09
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-01-18 21:14:50 +0700

    add click test

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+    const [isHello, setIsHello] = useState(true);
+
+    const changeTextHangler = () => {
+        setIsHello(prevState => !prevState);
+    }
+
     return (
         <div>
-            <h2>Hello world!</h2>
-            <p>It`s good to see you!</p>
+            <h2>Hello World!</h2>
+            <p>{isHello ? 'Hello p!' : 'Goodbye p!'}</p>
+            <button onClick={changeTextHangler}>Change text</button>
         </div>
     );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,15 +1,45 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("Renders Hello world as a text", ()=>{
+describe("Greeting component", () => {
+  test("Renders Hello world as a text", () => {
     // show component (Arrange)
-    render(<Greeting />)
+    render(<Greeting />);
 
-    // what we need to do (Act) 
+    // what we need to do (Act)
     //---
 
     //  what will be on the screen (Assert)
-    const helloWorldElement = screen.getByText("Hello world", {exact: false});
+    const helloWorldElement = screen.getByText("Hello world", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
-    
+  });
+
+  test("Rendering hello p text if button not click", () => {
+    render(<Greeting />);
+    const helloP = screen.getByText("Hello p", { exact: false });
+    expect(helloP).toBeInTheDocument();
+  });
+
+  test("Rendering goodbye p text if button click", () => {
+    render(<Greeting />);
+
+    const changeTextBtn = screen.getByRole("button");
+
+    userEvent.click(changeTextBtn);
+
+    const goodbyeP = screen.getByText("goodbye p", { exact: false });
+    expect(goodbyeP).toBeInTheDocument();
+  });
+
+  test("Rendering hello p text not show if button click", () => {
+    render(<Greeting />);
+
+    const changeTextBtn = screen.getByRole("button");
+
+    userEvent.click(changeTextBtn);
+
+    const notHelloP = screen.queryByText("hello p", { exact: false });
+    expect(notHelloP).toBeNull();
+  });
 });
